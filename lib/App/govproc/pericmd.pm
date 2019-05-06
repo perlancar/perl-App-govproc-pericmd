3b838ba9270d90361344d56b1048ef12f327b844
perlancar (on pc-office) <perlancar@gmail.com>
2019-05-06 18:28:12 +0700

    Initial commit

---
+++ lib/App/govproc/pericmd.pm
@@ -0,0 +1,19 @@
+package App::govproc::pericmd;
+
+# DATE
+# VERSION
+
+1;
+#ABSTRACT: govproc utility (Perinci::CmdLine version)
+
+=head1 DESCRIPTION
+
+This distribution carries L<govproc-pericmd>, the L<govproc> CLI utility based
+on L<Perinci::CmdLine>, mainly for testing or debugging only. Normal users
+should use the official B<govproc> from L<Proc::Govern> that has fewer
+dependencies.
+
+
+=head1 SEE ALSO
+
+L<Proc::Govern>
