044116e4dfaa0487a3470912e06b6255f0a65ae6
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2019-05-22 10:22:54 +0700

    Wrap original sub, to fix CLI output

--- lib/App/govproc/pericmd.pm
+++ lib/App/govproc/pericmd.pm
@@ -13,8 +13,9 @@ See the included script L<govproc-pericmd>.
 =head1 DESCRIPTION
 
 This distribution carries L<govproc-pericmd>, the L<govproc> CLI utility based
-on L<Perinci::CmdLine>. Normal users should still use the official B<govproc>
-from L<Proc::Govern> that has fewer dependencies.
+on L<Perinci::CmdLine>, mainly for testing. Normal users should still use the
+official B<govproc> from L<Proc::Govern> that is lighter and has fewer
+dependencies.
 
 
 =head1 SEE ALSO
