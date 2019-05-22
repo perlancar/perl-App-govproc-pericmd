7b4b34c21d81dc4ee1235527090dc682b5c3f081
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2019-05-22 09:53:09 +0700

    Initial commit

---
+++ lib/App/govproc/pericmd.pm
@@ -0,0 +1,16 @@
+package App::govproc::pericmd;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Perinci::CmdLine-based variant of govproc CLI
+
+=head1 SYNOPSIS
+
+See the included script L<govproc-pericmd>.
+
+
+=head1 SEE ALSO
+
+L<Proc::Govern>
